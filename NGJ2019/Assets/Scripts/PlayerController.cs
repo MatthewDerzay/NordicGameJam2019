5ae7d5e70edba74db8ec8a000a2b096788aeb06d
Brandt <brandtdudziak@gmail.com>
2019-04-26 21:06:16 +0200

    Animation

---
+++ NGJ2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed = 1;
+    private Rigidbody2D rb;
+    public Animator animator;
+    private float movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movement = 0;
+    }
+
+    void FixedUpdate()
+    {
+        movement = 0;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            movement = -1;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            movement = 1;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(movement * speed));
+        rb.velocity = new Vector2(movement, 0) * speed;
+    }
+}
