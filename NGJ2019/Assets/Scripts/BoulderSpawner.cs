946be359bd4ddbc018f5712a9ab4d0cce72d1df4
MatthewDerzay <mderzay@wisc.edu>
2019-04-27 19:16:14 +0200

    boulderspawner

---
+++ NGJ2019/Assets/Scripts/BoulderSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderSpawner : MonoBehaviour
+{
+
+    public LayerMask groundMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics2D.Linecast(transform.position, transform.position, 5))
+        {
+
+        }   
+    }
+}
