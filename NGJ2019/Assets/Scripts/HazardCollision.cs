c901f7536d243333324a0c44706ce3d4e711bcbe
Brandt <brandtdudziak@gmail.com>
2019-04-27 14:15:15 +0200

    Update HazardCollision.cs

--- NGJ2019/Assets/Scripts/HazardCollision.cs
+++ NGJ2019/Assets/Scripts/HazardCollision.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class HazardCollision : MonoBehaviour
 {
-    
+
+    private PlayerSpawner spawner;
+
+    private void Start() {
+        spawner = GameObject.Find("PlayerSpawn").GetComponent<PlayerSpawner>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Destroy(other.gameObject);
+        // reduce score
+        // death animation
+        Destroy(gameObject);
+        spawner.Spawn();
+    }
 }
