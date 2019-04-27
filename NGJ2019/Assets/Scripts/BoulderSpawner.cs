835c36a9110fb58d79e0fc0559cd4f022b990738
MatthewDerzay <mderzay@wisc.edu>
2019-04-27 19:47:34 +0200

    boulder stuff

--- NGJ2019/Assets/Scripts/BoulderSpawner.cs
+++ NGJ2019/Assets/Scripts/BoulderSpawner.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BoulderSpawner : MonoBehaviour
 {
-
+    public float total = 5;
     public LayerMask groundMask;
+    public GameObject boulder;
+
+    private float counter = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +19,14 @@ public class BoulderSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Physics2D.Linecast(transform.position, transform.position, 5))
+        if(counter < total)
         {
-
+            if (Physics2D.Linecast(transform.position, transform.position - Vector3.up * 4, groundMask))
+            {
+                GameObject go = Instantiate(boulder, transform.position, transform.rotation);
+                go.transform.parent = gameObject.transform;
+                counter += 1;
+            }
         }   
     }
 }
