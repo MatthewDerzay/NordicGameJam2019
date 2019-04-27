e1935bd8c6b4c703489e063315a9cb070f34c303
Brandt <brandtdudziak@gmail.com>
2019-04-27 13:45:37 +0200

    Game manager

---
+++ NGJ2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private PlayerSpawner spawner;
+
+    private void Start() {
+        spawner = GameObject.Find("PlayerSpawn").GetComponent<PlayerSpawner>();
+        spawner.Spawn();
+    }
+}
