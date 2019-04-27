fbca3a7f2148337405408a4df519963bf6ecdb9f
MatthewDerzay <mderzay@wisc.edu>
2019-04-27 12:01:47 +0200

    more fireball animations

--- NGJ2019/Assets/Scripts/Fireball.cs
+++ NGJ2019/Assets/Scripts/Fireball.cs
@@ -7,10 +7,12 @@ public class Fireball : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         StartCoroutine("DestroyFireball");
     }
 
@@ -24,6 +26,19 @@ public class Fireball : MonoBehaviour
     IEnumerator DestroyFireball()
     {
         yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        anim.SetBool("Dead", true);
+        StartCoroutine("DestroyFireballOnCollision");
+    }
+
+    IEnumerator DestroyFireballOnCollision()
+    {
+        yield return new WaitForSeconds(.75f);
         Destroy(gameObject);
     }
 }
